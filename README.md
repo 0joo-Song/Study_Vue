47bab33d7b310a001427734a14104574802083f0
0joo-Song <syoung7024@gmail.com>
2022-05-26 17:14:57 +0900

    v-on

--- README.md
+++ README.md
@@ -4,25 +4,37 @@ Vue 유튜브 강의 - 코딩애플
 
 
 ## 데이터 바인딩
-- - - 
  >데이터 값   {{ 데이터 이름 }}
  >속성         :속성="데이터 이름"
 
 
 ## html 반복문
-- - - 
 > <태그 v-for="작명 in 몇번반복" :key="작명">
 > key 안쓰면 에러남 용도 / 속성 값
 >
 > 숫자 위치에 array/object를 넣을 수 있음
 > 해당 array의 개수만큼 반복
 
-<pre><code>{<a v-for="menuNm in menu" :key="menuNm">{{ menuNm }}</a>}</code></pre>
+<pre><code><a v-for="menuNm in menu" :key="menuNm">{{ menuNm }}</a></code></pre>
 
-<pre><code>{<a v-for="(menuNm, i) in menu" :key="i">{{ menuNm }}</a>}</code></pre>
+<pre><code><a v-for="(menuNm, i) in menu" :key="i">{{ menuNm }}</a></code></pre>
 
 인덱스도 출력 가능
 
 
-#### 이벤트 핸들러
-- - -
+## 이벤트 핸들러
+- 버튼을 클릭했을때 이벤트 효과
+> v-on:click   
+>vue 방식 : @click
+
+<pre><code><button @click="rpCnt++">허위매물신고</button> <span>신고수 : {{ rpCnt }}</span></code></pre>
+<pre><code><button @click="rpCnt += 1">허위매물신고</button> <span>신고수 : {{ rpCnt }}</span></code></pre>
+
+- 마우스를 올렸을 때
+@mouseover
+
+등 다양한 함수가 있다.
+함수를 만들 때  methods : { 함수명(){} }
+
+
+## 이미지 넣는 법
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
+  <!-- 모달창 -->
+  <div class="black-bg">
+    <div class="white-bg">
+      <h4></h4>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="(menuNm, i) in menu" :key="i">{{ menuNm }}</a>
   </div>
 
   <div>
+    <img src="./assets/img/room1.jpg" class="room-img">
     <h4>{{ products[0] }}</h4>
-    <p>70 만원</p>
+    <p>30 만원</p>
+    <button @click="rpCnt[0]++">허위매물신고</button> <span>신고수 : {{ rpCnt[0] }}</span>
   </div>
    <div>
+    <img src="./assets/img/room2.jpg" class="room-img">
     <h4>{{ products[1] }}</h4>
     <p>40 만원</p>
+    <button @click="rpCnt[1]++">허위매물신고</button> <span>신고수 : {{ rpCnt[1] }}</span>
   </div>
    <div>
+    <img src="./assets/img/room3.jpg" class="room-img">
     <h4>{{ products[2] }}</h4>
     <p>50 만원</p>
+    <button @click="rpCnt[2]++">허위매물신고</button> <span>신고수 : {{ rpCnt[2] }}</span>
   </div>
   
 </template>
@@ -25,13 +38,21 @@ export default {
   // 데이터 보관함
   data(){
     return {
+      rpCnt : [0,0,0],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
       menu : ['Home', '아파트', '빌라/주택', '원룸'],
+    }
+  },
+  methods : {
+    clcickReport(){
+      
     }
   },
   components: {
   }
 }
+
+
 
 /* 데이터 바인딩 
    데이터 값   {{ 데이터 이름 }}
@@ -60,5 +81,10 @@ export default {
   color:white;
   padding: 10px;
   cursor: pointer;
+}
+
+.room-img {
+  width:60%;
+  margin-top: 40px;
 }
 </style>
